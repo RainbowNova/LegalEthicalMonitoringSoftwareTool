2d9f2dd9b1c91e1921eca5eb656116f4c7be6bc4
Jasper Smitz <2103073smitz@zuyd.nl>
2023-04-03 19:20:58 +0200

    Added base for active window grabber
    Basic implementation of the active window grabber. Simply reports to console when the active window changes.

--- web_and_app_info_logger.py
+++ web_and_app_info_logger.py
@@ -6,14 +6,32 @@
 ########################################################################################################################
 
 # Hier alle library imports
+import win32gui
 
 
 # Hieronder de relevante code
+def active_window_grabber():
+    """
+    Reports back whichever window is in focus in console
+    :return:
+    """
+    last_active_window = None
+
+    while True:
+        active_window = win32gui.GetForegroundWindow()
+        if active_window != last_active_window:
+            last_active_window = active_window
+            window_title = win32gui.GetWindowText(active_window)
+            if active_window == 0:
+                window_title = "Desktop"
+            if window_title != "":
+                print(f"Active window changed to: {window_title}")
+
+
 def main():
-    pass
+    active_window_grabber()
 
 
 if __name__ == '__main__':
     main()
 
-#Jaspers comment :sunglasses::thumbsup:
