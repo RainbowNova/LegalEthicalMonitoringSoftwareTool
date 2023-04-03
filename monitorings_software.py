6cb45aee6322984cfcfc596632d2b42570f8fa41
Keano <keanoschoenmaeckers@outlook.com>
2023-04-03 14:09:56 +0200

    Added main() code.
    Added main() code for each .py file.

---
+++ monitorings_software.py
@@ -0,0 +1,17 @@
+########################################################################################################################
+#               Dit is voor nu het main bestand voor de monitoringssoftware.                                           #
+#               Probeer zoveel mogelijk code in functies/classes te plaatsen i.p.v. los.                               #
+#               Als het nodig lijkt, bespreek met projectleden of er verschillende .py bestanden nodig zijn.           #
+#               - Keano (03-04-2023)                                                                                   #
+########################################################################################################################
+
+# Hier alle library imports
+
+
+# Hieronder de relevante code
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
